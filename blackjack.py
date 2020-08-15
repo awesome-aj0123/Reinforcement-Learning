7174a0fa6f71b2f8d340c3c1f990fd09e09a3054
Abhishek Joshi <56931504+awesome-aj0123@users.noreply.github.com>
2020-08-15 17:14:06 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,30 +1,17 @@
 import gym
-import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 env=gym.make('Blackjack-v0')
 
-"""Index Method"""
-
-def get_index(state):
-  # a: 2 -> 21
-  # b: 1 -> 10
-  # c: 0 or 1
- 
-  a, b, c = state
-  return ((20*10*c)+(10*(b-1))+(a-2))
-
-"""Loop to train the agent"""
-
-# 400 = number of states = hand_sum(2-21, 20 possibilities) * dealer_card(1-10, 10 possibilities) * useable ace?(0 or 1, 2 possibilities)
-table = np.zeros((400, env.action_space.n))
+table = np.zeros((32, 11, 2, env.action_space.n))
 
 episodes = 100000
-alpha = 0.05 # step size
+alpha = 0.01 # step size
 gamma = 0.9 # discount
 epsilon = 0.95 # for randomness
-decay = 0.999 # randomness decreases over time
+decay = 0.001 # randomness decreases over time
 
 rewards = []
 
@@ -33,35 +20,46 @@ for i in range(episodes):
   state = env.reset()
   done = False
 
+  state = list(state)
+  state[2] = int(state[2])
+  state[0] -= 2
+  state[1] -= 1
+  state = tuple(state)
+
   while not done:
 
-    index = get_index(state)
-    
+    # choosing action value
     random_num = np.random.random_sample()
     if random_num >= epsilon:
       #take the action with the max value
-      action = np.argmax(table[index]) # 0 = stick, 1 = hit
+      action = np.argmax(table[state]) # 0 = stick, 1 = hit
     else:
       #choose a random action
-      action = np.random.randint(2) # 0 = stick, 1 = hit
+      action = env.action_space.sample() # 0 = stick, 1 = hit
 
-    new_state,reward,done,_=env.step(action)
+    new_state, reward, done, _ = env.step(action)
 
-    # Update rule
-    target = reward + gamma * np.max(table[get_index(new_state)])
-    table[index, action] = table[index, action] + alpha*(target - table[index,action])
+    new_state = list(new_state)
+    new_state[2] = int(new_state[2])
+    new_state[0] -= 2
+    new_state[1] -= 1
+    new_state = tuple(new_state)
+
+    target = reward + gamma * np.max(table[new_state])
+    table[state][action] = table[state][action] + alpha*(target - table[state][action])
 
     state = new_state
-    rewards.append(reward)
 
-    epsilon *= decay
+  rewards.append(reward)
+  epsilon *= 0.01 + np.exp(-decay*i)
 
-count = 0
-range = 20
-averages = []
-while count < episodes-range:
-  averages.append(np.mean(rewards[count:count+range]))
-  count+=range
+sums = 0
+avg_rewards = []
+for i in range(len(rewards)):
+  if rewards[i] == 1:
+    sums+=1;
+  avg_rewards.append(sums / (i+1));
 
-plt.plot(percentages)
+plt.ylim(0, 0.5)
+plt.plot(avg_rewards)
 plt.show()
